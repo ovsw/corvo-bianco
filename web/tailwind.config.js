244252b328d8e44ff33875c2fecced7f90590264
Ovi Savescu <ovi@ovswebsites.com>
2019-06-16 11:09:25 +0300

    test tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brandp: 'red',
+        spacing: {
+          '96': '24rem',
+          '128': '32rem'
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
