7e1c87802b5672770965ecc6019e71d730939c6a
Ovi Savescu <ovi@ovswebsites.com>
2019-07-19 14:16:21 +0300

    removed old web folder

--- web_old/src/lib/image-url.js
+++
@@ -1,8 +0,0 @@
-import imageUrlBuilder from '@sanity/image-url'
-import sanityConfig from '../../../studio/sanity.json'
-
-const builder = imageUrlBuilder(sanityConfig.api)
-
-export function imageUrlFor(source) {
-  return builder.image(source)
-}
