467c697a73ec39ef4cf2deae9200557114c72447
Ovi Savescu <ovi@ovswebsites.com>
2019-07-23 02:31:33 +0300

    added social icons in topbar

--- web/src/components/Header/TopBar.js
+++ web/src/components/Header/TopBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
+import { FaFacebookSquare, FaInstagram, FaTwitter, FaWhatsapp, FaRegEnvelope } from 'react-icons/fa'
+
 // images
 import ChalkBorderSource from '../../images/chalk-squalre.png'
 
@@ -14,7 +16,6 @@ const Wrapper = styled.div`
   border-bottom: 5px solid white;
   border-top: 5px solid white;
   border-image-outset: 0.5;
-  ${tw``};
   p {
     color: white;
     ${tw`inline-block  font-chalk text-xl p-2 pt-4`};
@@ -23,11 +24,49 @@ const Wrapper = styled.div`
     border-radius: 0 15px 15px 0;
   }
 `
+const ColWrapper = styled.div`
+  ${tw`flex`};
+`
+const LeftCol = styled.div`
+  ${tw`flex-1`};
+`
+const RightCol = styled.div`
+  ${tw`flex-1 text-right`};
+`
+const SocialLink = styled.a`
+  ${tw`mr-4`};
+  &:hover {
+    ${tw`text-white`};
+  }
+`
 
 const TopBar = () => (
   <Wrapper>
     <Container>
-      <p>Fast, tasty, healthy, NOT fast-food.</p>
+      <ColWrapper>
+        <LeftCol>
+          <p>Fast, tasty, healthy, NOT fast-food. </p>
+        </LeftCol>
+        <RightCol>
+          <p>
+            <SocialLink href="https://www.google.com" rel="noopener noreferrer" target="_blank">
+              <FaFacebookSquare />
+            </SocialLink>
+            <SocialLink href="https://www.google.com" rel="noopener noreferrer" target="_blank">
+              <FaInstagram />
+            </SocialLink>
+            <SocialLink href="https://www.google.com" rel="noopener noreferrer" target="_blank">
+              <FaTwitter />
+            </SocialLink>
+            <SocialLink href="https://www.google.com" rel="noopener noreferrer" target="_blank">
+              <FaWhatsapp />
+            </SocialLink>
+            <SocialLink href="https://www.google.com" rel="noopener noreferrer" target="_blank">
+              <FaRegEnvelope />
+            </SocialLink>
+          </p>
+        </RightCol>
+      </ColWrapper>
     </Container>
   </Wrapper>
 )
