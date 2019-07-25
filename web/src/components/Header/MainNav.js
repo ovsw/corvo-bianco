f8ed18ee002d4de684c71d06ea1d134c5458f630
Ovi Savescu <ovi@ovswebsites.com>
2019-07-25 09:21:17 +0300

    removed links to non-existant pages for now

--- web/src/components/Header/MainNav.js
+++ web/src/components/Header/MainNav.js
@@ -13,7 +13,7 @@ const StyledMenu = styled.ul`
 const MenuItem = styled.li`
   ${tw`w-1/3 border`};
   ${tw`flex`};
-  border: 5px solid white;
+  border: 5px solid black;
   border-image: url(${ChalkBorderSource}) 70;
   border-image-repeat: round;
   border-image-outset: 0.5;
@@ -22,7 +22,7 @@ const MenuItem = styled.li`
     ${tw`flex w-full items-center justify-center`};
     ${tw`m-0 px-0 md:px-2  text-white text-sm uppercase sm:text-base xl:text-xl font-chalk md:tracking-wide`};
     span {
-      border-left: 6px dotted #161719;
+      border-left: 6px solid #161719;
       ${tw`m-0 py-1`};
     }
     &:hover {
@@ -42,27 +42,27 @@ const MainNav = () => {
   const MenuItems = [
     {
       name: 'Menu',
-      url: '/menu',
+      url: '/',
     },
     {
       name: 'Location',
-      url: '/location',
+      url: '/',
     },
     {
       name: 'Reviews',
-      url: '/reviews',
+      url: '/',
     },
     {
       name: 'About',
-      url: '/about-us',
+      url: '/',
     },
     {
       name: 'News',
-      url: '/news',
+      url: '/',
     },
     {
       name: 'Contact',
-      url: '/contact',
+      url: '/',
     },
   ]
 
