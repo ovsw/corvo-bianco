2e08aeb8381ed20fe567a6ec7048cd5726e8f2d1
Ovi Savescu <ovi@ovswebsites.com>
2019-08-27 15:04:36 +0300

    topbar text style adjustments

--- web/src/components/Header/TopBar.js
+++ web/src/components/Header/TopBar.js
@@ -18,7 +18,7 @@ const Wrapper = styled.div`
   border-image-outset: 1px;
   p {
     color: white;
-    ${tw`inline-block text-sm md:text-md font-typewritter text-sm p-2 pt-4 md:pl-4`};
+    ${tw`inline-block text-xs md:text-md font-typewritter text-xs md:text-sm p-2 pt-4 md:pl-4`};
     opacity: 1;
     border-radius: 0 15px 15px 0;
   }
@@ -26,22 +26,21 @@ const Wrapper = styled.div`
 const ColWrapper = styled.div`
   ${tw`flex items-center justify-between`};
   @media (min-width: 600px) {
-    padding-left: 150px;
+    margin-left: 150px;
   }
   @media (min-width: 900px) {
-    padding-left: 250px;
+    margin-left: 250px;
   }
 `
 const LeftCol = styled.div`
-  ${tw`md:w-3/5 lg:w-3/4 `};
+  ${tw`md:w-3/4  `};
 `
 const RightCol = styled.div`
-  ${tw` text-right md:w-2/5 lg:w-1/4`};
+  ${tw` text-right md:w-1/4 `};
   a {
     ${tw`flex items-center md:inline-block hidden md:text-3xl relative`};
     top: 3px;
-    &:first-child,
-    &:nth-child(2) {
+    &:first-child {
       ${tw`inline-block`};
     }
   }
@@ -52,7 +51,7 @@ const TopBar = () => (
     <Container>
       <ColWrapper>
         <LeftCol>
-          <p>Tagline from Kay goes here.</p>
+          <p>Woodfired Neopolitan Pizza Truck - Tampa Bay Area</p>
         </LeftCol>
         <RightCol>
           <div>
