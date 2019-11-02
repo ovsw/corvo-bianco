97e83bb898742dbdf751e38281a49bd8378d972a
Ovi Savescu <ovi@ovswebsites.com>
2019-11-02 11:22:38 +0200

    remove events listing limit

--- web/src/pages/events.js
+++ web/src/pages/events.js
@@ -70,7 +70,7 @@ export const query = graphql`
       _rawBody
     }
 
-    events: allSanityEvent(limit: 6, sort: { fields: [publishedAt], order: ASC }) {
+    events: allSanityEvent(limit: 100, sort: { fields: [publishedAt], order: ASC }) {
       edges {
         node {
           id
