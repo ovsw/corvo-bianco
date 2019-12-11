6e9d674fa95ef0c99b8832635cae93721453c527
Ovi Savescu <ovi@ovswebsites.com>
2019-12-11 12:28:15 +0200

    added schema.org Schemas  from Kay

--- web/src/components/SEO-orig.jsx
+++ web/src/components/SEO-orig.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-// import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import config from '../../config/website'
 
-import {
-  websiteSchema,
-  localBusinessSchema,
-  professionalServiceSchema,
-  breadCrumbsList,
-  organizationSchema,
-  reviewSchema,
-} from './schemas'
-
 const SEO = ({ seoTitle, seoDescription }) => {
-  // const data = useStaticQuery(
-  //   graphql`
-  //     query SEO {
-  //       site {
-  //         buildTime(formatString: "YYYY-MM-DD")
-  //       }
-  //     }
-  //   `
-  // )
+  const data = useStaticQuery(
+    graphql`
+      query SEO {
+        site {
+          buildTime(formatString: "YYYY-MM-DD")
+        }
+      }
+    `
+  )
 
   const title = seoTitle
   const description = seoDescription
@@ -29,6 +20,65 @@ const SEO = ({ seoTitle, seoDescription }) => {
   const realPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
   const homeURL = `${config.siteUrl}${realPrefix}`
   const image = `${homeURL}${config.siteLogo}`
+
+  // schema.org in JSONLD format
+  // https://developers.google.com/search/docs/guides/intro-structured-data
+  // You can fill out the 'author', 'creator' with more data or another type (e.g. 'Organization')
+
+  const schemaOrgWebPage = {
+    '@context': 'http://schema.org',
+    '@type': 'WebPage',
+    url: homeURL,
+    headline: config.siteHeadline,
+    inLanguage: 'en',
+    mainEntityOfPage: homeURL,
+    description: data.seoDescription,
+    name: data.seoTitle,
+    author: {
+      '@type': 'Organization',
+      name: 'OVS Websites',
+    },
+    copyrightHolder: {
+      '@type': 'Person',
+      name: 'Dena Leigh',
+    },
+    copyrightYear: '2019',
+    creator: {
+      '@type': 'Person',
+      name: 'Dena Leigh',
+    },
+    publisher: {
+      '@type': 'Person',
+      name: 'Corvo Bianco',
+    },
+    datePublished: '2019-01-17',
+    dateModified: data.site.buildTime,
+    image: {
+      '@type': 'ImageObject',
+      url: image,
+    },
+  }
+
+  // Initial breadcrumb list
+
+  const itemListElement = [
+    {
+      '@type': 'ListItem',
+      item: {
+        '@id': homeURL,
+        name: 'Homepage',
+      },
+      position: 1,
+    },
+  ]
+
+  const breadcrumb = {
+    '@context': 'http://schema.org',
+    '@type': 'BreadcrumbList',
+    description: 'Breadcrumbs list',
+    name: 'Breadcrumbs',
+    itemListElement,
+  }
 
   return (
     <Helmet>
@@ -58,12 +108,8 @@ const SEO = ({ seoTitle, seoDescription }) => {
       <meta name="twitter:image" content={image} />
       <meta name="twitter:image:alt" content={description} />
       <meta name="google-site-verification" content="QS8GjLT3XOeeEU9Mc6NgrGNBUPlH144c9LGV_RXQWKk" />
-      <script type="application/ld+json">{JSON.stringify(websiteSchema)}</script>
-      <script type="application/ld+json">{JSON.stringify(localBusinessSchema)}</script>
-      <script type="application/ld+json">{JSON.stringify(professionalServiceSchema)}</script>
-      <script type="application/ld+json">{JSON.stringify(breadCrumbsList)}</script>
-      <script type="application/ld+json">{JSON.stringify(organizationSchema)}</script>
-      <script type="application/ld+json">{JSON.stringify(reviewSchema)}</script>
+      <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>
+      <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script>
     </Helmet>
   )
 }
